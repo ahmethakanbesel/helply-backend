269d184b7936c0e0e0a27b5a22004fe34282ed28
Ahmet Hakan <ahmethakan.besel@gmail.com>
2022-05-23 21:36:07 +0300

    Update CreateCustomerLicense

--- app/controllers/customer_license_controller.go
+++ app/controllers/customer_license_controller.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/helply/backend/app/dto"
 	"github.com/helply/backend/app/models"
+	"github.com/helply/backend/pkg/helpers"
 	"github.com/helply/backend/platform/database"
 )
 
@@ -17,25 +19,30 @@ import (
 // @Param category_id body int true "Category ID"
 // @Success 200 {object} models.CustomerLicense
 // @Security ApiKeyAuth
-// @Router /api/v1/customerlicenses [post]
+// @Router /api/v1/customer-licenses [post]
 func CreateCustomerLicense(ctx *fiber.Ctx) error {
-	type NewCustomerLicense struct {
-		CustomerID uint32 ` json:"customer_id" `
-		LicenseID  uint32 `json:"license_id" `
-	}
 	db := database.Connection()
-	newCustomerLicense := new(NewCustomerLicense)
-	if err := ctx.BodyParser(newCustomerLicense); err != nil {
-		return ctx.Status(500).JSON(fiber.Map{"status:": "error", "message:": "Invalid input data.", "data:": err})
+	data := new(dto.CustomerLicenseDto)
+	if err := ctx.BodyParser(data); err != nil {
+		return ctx.Status(400).JSON(fiber.Map{"status:": "error", "message:": "Invalid data given.", "data:": err})
+	}
+	claims, err := helpers.ExtractTokenMetadata(ctx)
+	if err != nil {
+		return ctx.Status(404).JSON(fiber.Map{"status": "error", "message": "Couldn't get user", "data": err})
 	}
 	customerLicense := new(models.CustomerLicense)
-	customerLicense.CustomerID = newCustomerLicense.CustomerID
-	customerLicense.LicenseID = newCustomerLicense.LicenseID
+	customerLicense.CustomerID = claims.ID
+	license := &models.License{}
+	err = database.Connection().First(&license, "code = ?", data.LicenseCode).Error
+	if err != nil {
+		return ctx.Status(404).JSON(fiber.Map{"status": "error", "message": "License code is not valid.", "data": err})
+	}
+	customerLicense.LicenseID = license.ID
 	if err := db.Create(&customerLicense).Error; err != nil {
-		return ctx.Status(500).JSON(fiber.Map{"status:": "error", "message:": "Couldn't create customer License.", "data:": err})
+		return ctx.Status(500).JSON(fiber.Map{"status:": "error", "message:": "Couldn't create Customer License.", "data:": err})
 	}
 
-	return ctx.JSON(fiber.Map{"status:": "success", "message:": "customer License created", "data:": customerLicense})
+	return ctx.JSON(fiber.Map{"status:": "success", "message:": "Customer license created", "data:": customerLicense})
 
 }
 
@@ -46,7 +53,7 @@ func DeleteCustomerLicense(ctx *fiber.Ctx) error {
 		return err
 	}
 
-	return ctx.JSON(fiber.Map{"status": "success", "message": "customer License deleted."})
+	return ctx.JSON(fiber.Map{"status": "success", "message": "Customer License deleted."})
 }
 
 func GetCustomerLicenses(ctx *fiber.Ctx) error {
---
+++ app/dto/customer_license_dto.go
@@ -0,0 +1,5 @@
+package dto
+
+type CustomerLicenseDto struct {
+	LicenseCode string `json:"license_code"`
+}
