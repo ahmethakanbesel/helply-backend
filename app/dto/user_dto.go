4f175d9a7bd0e2d4d664e560541cbfa29fc102e4
Ahmet Hakan <ahmethakan.besel@gmail.com>
2022-05-26 16:24:07 +0300

    Update UserDTO

--- app/dto/user_dto.go
+++ app/dto/user_dto.go
@@ -4,4 +4,12 @@ type UserDTO struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Phone    string `json:"phone"`
+	PhotoID  string `json:"photo_id"`
+}
+
+type UserLoginDTO struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
