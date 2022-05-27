413db5b0bf9aae7875b05cd5a3d26fa59ab612f2
Ahmet Hakan <ahmethakan.besel@gmail.com>
2022-05-27 21:49:02 +0300

    Complete admin dashboard pie chart

--- app/controllers/dashboard_controller.go
+++ app/controllers/dashboard_controller.go
@@ -12,5 +12,8 @@ func GetAdminStats(ctx *fiber.Ctx) error {
 	database.Connection().Model(&models.User{}).Count(&stats.WidgetData.UserCount)
 	database.Connection().Model(&models.Product{}).Count(&stats.WidgetData.ProductCount)
 	database.Connection().Model(&models.License{}).Count(&stats.WidgetData.LicenseCount)
+	var licenseStats []models.LicenseStats
+	database.Connection().Raw("SELECT p.name as id, p.name as label, count(*) as value FROM products p, licenses l WHERE p.id=l.product_id GROUP BY p.name, p.id").Scan(&licenseStats)
+	stats.LicenseStats = licenseStats
 	return ctx.JSON(fiber.Map{"status:": "success", "message:": "", "data": stats})
 }
--- app/models/stats_model.go
+++ app/models/stats_model.go
@@ -1,7 +1,8 @@
 package models
 
 type Stats struct {
-	WidgetData WidgetData `json:"widget_data"`
+	WidgetData   WidgetData     `json:"widget_data"`
+	LicenseStats []LicenseStats `json:"license_stats"`
 }
 
 type WidgetData struct {
@@ -9,4 +10,10 @@ type WidgetData struct {
 	UserCount    int64 `json:"user_count"`
 	ProductCount int64 `json:"product_count"`
 	LicenseCount int64 `json:"license_count"`
+}
+
+type LicenseStats struct {
+	ID    string `json:"id"`
+	Label string `json:"label"`
+	Value int64  `json:"value"`
 }
